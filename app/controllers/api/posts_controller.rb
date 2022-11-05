614f69153b9af889c446f2ebbfced35be4a02650
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-11-05 12:05:23 +0500

    APIs working now

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::PostsController < ApplicationController
-  before_action :authenticate_request
+  #before_action :authenticate_request
   def index
     @posts = Post.where(author_id: params[:user_id])
     if @posts.empty?
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   end
   root to: "users#index"
   post 'authenticate', to: 'authentication#authenticate'
+
   namespace :api do
     resources :users do 
       resources :posts 
@@ -21,3 +22,25 @@ Rails.application.routes.draw do
     end
   end
 end
+
+# Rails.application.routes.draw do
+#   # devise_for :users, sign_out_via: [:get, :post]
+#   resources :users do
+#     resources :posts
+#    end
+#    resources :posts do
+#     resources :comments
+#     resources :likes
+#    end
+  
+#    root 'users#index'
+
+#    namespace :api do  
+#     resources :posts, only: %i[create index show]
+#     resources :users, only: [:index, :show] do
+#       resources :posts, only: [:index] do
+#         resources :comments, only: [:index]
+#       end
+#     end
+#   end
+# end
